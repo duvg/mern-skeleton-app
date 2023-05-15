bec0be9ac904152d08a2aa0d6da199879059ea6d
Duviel Garcia <duviel7@gmail.com>
2023-05-14 22:36:50 -0500

    feat_2: added crud for user, error handler and authentications methods

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,91 @@
+import User from '../models/user.model';
+import extend from 'lodash';
+import errorHandler from './../helpers/dbErrorHandler';
+
+const create = async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    return res.status(200).json({
+      message: 'Successfully signed up!'
+    });
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err)
+    });
+  }
+};
+
+const list = async (req, res) => {
+  try {
+    let users = await User.find().select('name email updated created');
+    res.json(users);
+  } catch (err) {
+    return res.status('400').json({
+      error: errorHandler.getErrorMessage(err)
+    })
+  }
+};
+
+const userById = async (req, res, next, id) => {
+  try {
+    let user = await User.findOne({_id: id});
+    if(!user) {
+      return res.status(400).json({
+        error: 'User not found'
+      });
+    }
+    req.profile = user;
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      error: "Could not retrieve user"
+    });
+  }
+};
+
+const read = (req, res) => {
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+  req.name = 'ss';
+  return res.json(req.profile);
+};
+const update = async (req, res, next) => {
+  try {
+    let user = req.profile;
+    user = extend(user, req.body);
+    user.updated = Date.now();
+    await user.save();
+    user.hashed_password = '';
+    user.salt = '';
+    res.json(user);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err)
+    });
+  }
+};
+
+const remove = async (req, res, next) => {
+  try {
+    let user = req.profile;
+    let deleteUser =  await user.remove();
+    deletedUser.hashed_password = '';
+    deleteUser.salt = '';
+    res.json(deletedUser);
+  } catch(err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err)
+    });
+  }
+};
+
+export default {
+  create,
+  list,
+  read,
+  remove,
+  userById,
+  update
+};
